1f1cc4d7844bd3de1dcae5707030beabdee04baf
Black貓①呺 <jha334201553@163.com>
2020-06-24 15:05:01 +0800

    添加守护进程，编写项目说明

--- WebService/main.cpp
+++ WebService/main.cpp
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include "AnalyzeRequestHttp.h"
 #include "ServiceProvider.h"
+#include "common.h"
+#include <Windows.h>
+#include <tchar.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -90,8 +93,119 @@ static INT WebService()
     return bRetVal;
 }
 
-int main(int argc, char* argv[])
+// 运行进程 
+static HANDLE RunProcess(LPTSTR lpCmd)
 {
-    WebService();
+    STARTUPINFO si = {sizeof(STARTUPINFO)};  
+    PROCESS_INFORMATION pi;  
+
+    si.wShowWindow = SW_SHOW; 
+    si.dwFlags = STARTF_USESHOWWINDOW; 
+    if(CreateProcess(
+        NULL, 
+        lpCmd, 
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi))  
+    {  
+        CloseHandle(pi.hThread);
+        return pi.hProcess;
+    } 
+    return NULL;
+}
+
+// 守护进程 
+static void DaemonProcess(DWORD pid, LPTSTR lpcmdline)
+{
+    if (pid != 0)
+    {
+        // 已有进程等待 
+        HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, pid);
+        WaitForSingleObject(hProcess, INFINITE);
+        CloseHandle(hProcess);
+    }
+
+    // 启动进程并等待 
+    LPTSTR lpCmd = (LPTSTR)malloc(MAX_PATH*2*sizeof(TCHAR));
+    if (lpCmd == NULL)
+    {
+        return;
+    }
+
+    lpCmd[0] = TEXT('"');
+    GetModuleFileName(GetModuleHandle(NULL), lpCmd+1, MAX_PATH);
+    int len = _tcslen(lpCmd);
+    wsprintf(lpCmd+len, lpcmdline, GetCurrentProcessId());
+    while(TRUE)
+    {
+        HANDLE hProcess = RunProcess(lpCmd);
+        if (hProcess != NULL)
+        {
+            WaitForSingleObject(hProcess, INFINITE);
+            CloseHandle(hProcess);
+        }
+    }
+    free(lpCmd);
+}
+
+// web服务进程监控守护进程是否运行 
+static DWORD WINAPI MonitorParentProcess(LPVOID lparam)
+{
+    DWORD pid = (DWORD)lparam;
+    DaemonProcess(pid, TEXT("\" /pid %u"));
+    return 0;
+}
+
+static DWORD StringToDword(PTCH str)
+{
+    if ( memicmp(str, TEXT("0x"), 2*sizeof(TCHAR)) == 0 )
+    {
+        return _tcstol(str+2, NULL, 16);
+    }
+    else
+    {
+        return _tcstol(str, NULL, 10);
+    }
+}
+
+
+INT _tmain(INT argc, TCHAR* argv[])
+{
+    BOOL bRunServer = FALSE;
+    DWORD ppid = 0;
+    for (int i=1; i<argc; i++)
+    {
+        if ( 
+            memicmp(argv[i], TEXT("/runserver"), 10*sizeof(TCHAR) ) == 0 ||
+            memicmp(argv[i], TEXT("-runserver"), 10*sizeof(TCHAR) ) == 0 
+            )
+        {
+            bRunServer = TRUE;
+        }
+        else if ( 
+            memicmp(argv[i], TEXT("/pid"), 4*sizeof(TCHAR) ) == 0 ||
+            memicmp(argv[i], TEXT("-pid"), 4*sizeof(TCHAR) ) == 0 
+            )
+        {
+            ppid = StringToDword(argv[++i]);
+        }
+    }
+    if (bRunServer)
+    {
+        // 启动监控守护进程 
+        CloseHandle(CreateThread(NULL, 0, MonitorParentProcess, (LPVOID)ppid, 0, NULL));
+        // 启动web服务 
+        WebService();
+    }
+    else
+    {
+        // 监控web服务 
+        DaemonProcess(ppid, TEXT("\" /runserver /pid %u"));
+    }
     return 0;
 }
